0da466eddb557f1e80ecfb2eeccbf725386f9dd1
Jonnii Ringo <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-12-18 10:03:48 -0800

    Ternery solution

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,6 +6,7 @@ function Login(){
       <form className= "form">
         <input type="text" placeholder="Username" />
         <input type="password" placeholder= "Password" /> 
+        <button type="submit">Login</button>
       </form>
     );
   }
